b8031650c4dc314b13d57f9caa0c26416c5eccea
Atulya Kadur <86684438+AtulyaK@users.noreply.github.com>
2022-03-13 09:49:08 -0700

    Update multiplicationtable.py

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -1,3 +1,4 @@
+# I needed a multiplication table for a project. I developed this in order to help solve the issue of multiplying 44 so many times.
 num = 44
 print("Multiplicaiton table of", num)
 for i in range(23,228):
