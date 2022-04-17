274048fdb9507c3484fd27428c7a6f33f05cb687
Atulya Kadur <atulya.kadur@gmail.com>
2022-04-17 13:28:17 -0700

    Create remainder division.py

---
+++ python notes/remainder division.py
@@ -0,0 +1,8 @@
+import math
+import random
+a = random.randint(1,10)
+b= random.randint(1,10)
+z = a%b
+while z > 0:
+    print(a, "/", b ,"=")
+# i need the function of print to only loop so that the division makes floats
